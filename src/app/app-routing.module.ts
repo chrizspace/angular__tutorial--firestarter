de8b3d42f1b02fd45f11098055ed18910c95a8e3
Chriz <cieslikkrzysztof@gmail.com>
2020-07-13 16:39:59 +0200

    AuthGuard Preparation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-
-
+import { AuthGuard} from './user/auth.guard'
+ 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   {
